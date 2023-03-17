c1d3a93db29054d1de6d15d114b04b740ac4cba1
Alden Kim <aldenk51@gmail.com>
2023-03-16 20:37:43 -0400

    format document

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,10 @@
  * @returns {string} Rock if x<0.33, Paper if x<0.66, else Scissor 
  */
 function getComputerChoice() {
-    let x = Math.random();
-    if (x < 0.33) return "rock"
-    if (x < 0.66) return "paper"
-    return "scissor"
+  let x = Math.random();
+  if (x < 0.33) return "rock"
+  if (x < 0.66) return "paper"
+  return "scissor"
 }
 
 
@@ -17,20 +17,20 @@ function getComputerChoice() {
  * @returns {string} The result of the showdown
  */
 function playRound(playerSelection, computerSelection) {
-    console.log(playerSelection);
-    console.log(computerSelection)
-    if(playerSelection == computerSelection) {
-        console.log("It was a tie!");
-    } else if ((playerSelection == "rock" && computerSelection == "scissor") || 
-            (playerSelection == "paper" && computerSelection == "rock") ||
-            (playerSelection == "scissor" && computerSelection == "paper")) {
-        document.querySelector('#playerScore').textContent ++;
-        console.log("You win!");
-    } else {
-        document.querySelector('#compScore').textContent++;
-        console.log("You lose!");
-    }
-    checkWinner();
+  console.log(playerSelection);
+  console.log(computerSelection)
+  if (playerSelection == computerSelection) {
+    console.log("It was a tie!");
+  } else if ((playerSelection == "rock" && computerSelection == "scissor") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissor" && computerSelection == "paper")) {
+    document.querySelector('#playerScore').textContent++;
+    console.log("You win!");
+  } else {
+    document.querySelector('#compScore').textContent++;
+    console.log("You lose!");
+  }
+  checkWinner();
 }
 
 
@@ -38,27 +38,27 @@ function playRound(playerSelection, computerSelection) {
  * Checks the score to see if a winner has appeared
  */
 function checkWinner() {
-    if(document.querySelector('#playerScore').textContent != 5 && document.querySelector('#compScore').textContent != 5) return;
-    
-    if (document.querySelector('#playerScore').textContent == 5) {
-        document.querySelector('#results').textContent = "Player wins!";
-    } else {
-        document.querySelector('#results').textContent = "Computer wins!";
-    }
-    document.querySelectorAll('button').forEach(button => {button.removeEventListener('click', playerChoice)});
-    return;
-}   
+  if (document.querySelector('#playerScore').textContent != 5 && document.querySelector('#compScore').textContent != 5) return;
+
+  if (document.querySelector('#playerScore').textContent == 5) {
+    document.querySelector('#results').textContent = "Player wins!";
+  } else {
+    document.querySelector('#results').textContent = "Computer wins!";
+  }
+  document.querySelectorAll('button').forEach(button => { button.removeEventListener('click', playerChoice) });
+  return;
+}
 
 
 /**
  * Interacts with buttons
  */
 document.querySelector('#reload').addEventListener("click", () => {
-    location.reload();
+  location.reload();
 });
-document.querySelectorAll('button').forEach(button => {button.addEventListener('click', playerChoice)});
+document.querySelectorAll('button').forEach(button => { button.addEventListener('click', playerChoice) });
 
 function playerChoice(e) {
-    let playerSelection = e.target.id;
-    playRound(playerSelection, getComputerChoice());
+  let playerSelection = e.target.id;
+  playRound(playerSelection, getComputerChoice());
 }
